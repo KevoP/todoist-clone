73fb6c30f843d6fdbf32220b46d38a97f7e4f015
zgprunty <kevinpaulprunty@gmail.com>
2019-11-28 14:26:12 +0000

    initial commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Header = () => (
+    <header>
+        <nav>
+            <div className="logo">
+                <img src="" alt=""/>
+            </div>
+            <div className="settings">
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
